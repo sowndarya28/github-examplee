79ec262e78283fde15a5de8d894a5d626544db2c
sowndarya28 <64298455+sowndarya28@users.noreply.github.com>
2020-04-26 11:44:44 +0530

    Add files via upload

---
+++ capstone project.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will mainly be used for capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
